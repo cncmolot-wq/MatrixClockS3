5e3812c860c9da5721c5210f52cfec85805d029a
cncmolot-wq <cncmolot@gmail.com>
2026-08-16 02:56:40 +0300

    Patch only executable RC16 negative simulation occurrence

--- ci/miningsniper/run_rebuild_validator_v4.ps1
+++ ci/miningsniper/run_rebuild_validator_v4.ps1
@@ -8,8 +8,10 @@ $old='$simFail=$simPass.Clone();$simFail.controlEffect=$false'
 $new='$simFail=[ordered]@{productive40=$true;lowerProductiveCount=1;timingOk=$true;controlEffect=$false;poolOk=$true;threadControl=$true}'
 $first=$text.IndexOf($old,[StringComparison]::Ordinal)
 $last=$text.LastIndexOf($old,[StringComparison]::Ordinal)
-if($first-lt0-or$first-ne$last){throw ('Expected one OrderedDictionary Clone simulation line, first='+$first+' last='+$last)}
-$patched=$text.Substring(0,$first)+$new+$text.Substring($first+$old.Length)
+if($first-lt0-or$last-lt0-or$first-eq$last){throw ('Expected distinct oldState/newState Clone occurrences, first='+$first+' last='+$last)}
+$middle=$text.IndexOf($old,$first+$old.Length,[StringComparison]::Ordinal)
+if($middle-ne$last){throw ('Expected exactly two Clone occurrences, middle='+$middle+' last='+$last)}
+$patched=$text.Substring(0,$last)+$new+$text.Substring($last+$old.Length)
 [IO.File]::WriteAllText($tmp,$patched,[Text.UTF8Encoding]::new($false))
 $t=$null;$e=$null;[void][Management.Automation.Language.Parser]::ParseFile($tmp,[ref]$t,[ref]$e)
 if($e.Count-gt0){throw ('Generated outer validator ParseFile failed: '+(($e|ForEach-Object{$_.ErrorId+':'+$_.Message})-join' | '))}
